63a15227dd65b7f34ff1886587121b8eb92af4b2
Meng-Yuan Huang <myh@live.com>
2026-02-12 22:04:55 +0800

    0.2.0

--- dist/DailyOperationalStatisticsOfReservoir.d.ts
+++ dist/DailyOperationalStatisticsOfReservoir.d.ts
@@ -1,17 +1,17 @@
 import type { ReservoirConditionData } from './ReservoirConditionData';
 export interface DailyOperationalStatisticsOfReservoir {
-    catchmentarearainfall: number;
     crossflow: number;
-    deadstoragelevel: number;
     capacity: number;
-    fullwaterlevel: number;
-    inflowvolume: number;
     outflow: number;
     outflowdischarge: number;
     outflowtotal: number;
-    recordtime: string;
     regulatorydischarge: number;
     reservoiridentifier: string;
     reservoirname: string;
     latestwaterdata: ReservoirConditionData | undefined;
+    basinrainfall: string;
+    datetime: string;
+    dwl: string;
+    inflow: string;
+    nwlmax: string;
 }
--- src/DailyOperationalStatisticsOfReservoir.ts
+++ src/DailyOperationalStatisticsOfReservoir.ts
@@ -1,18 +1,19 @@
 import type { ReservoirConditionData } from './ReservoirConditionData';
 
 export interface DailyOperationalStatisticsOfReservoir {
-  catchmentarearainfall: number;
   crossflow: number;
-  deadstoragelevel: number;
   capacity: number;
-  fullwaterlevel: number;
-  inflowvolume: number;
   outflow: number;
   outflowdischarge: number;
   outflowtotal: number;
-  recordtime: string;
   regulatorydischarge: number;
   reservoiridentifier: string;
   reservoirname: string;
   latestwaterdata: ReservoirConditionData | undefined;
+
+  basinrainfall: string;
+  datetime: string; // ISO 8601
+  dwl: string;
+  inflow: string;
+  nwlmax: string;
 }
